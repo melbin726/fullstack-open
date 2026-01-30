ed3d3e34f81814a5ba77fb7c01f4fd9de3054b96
melbin726 <141119619+melbin726@users.noreply.github.com>
2026-01-30 17:12:03 +0530

    exercise part1 1.9

---
+++ part1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+
+ 
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const average = (props.good - props.bad) / all
+  const positive = (props.good / all) * 100
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+     
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
