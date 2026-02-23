5de21e465307618f4cb774b30bb3e0103c92477f
melbin726 <141119619+melbin726@users.noreply.github.com>
2026-02-23 12:49:03 +0530

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({ name }) => <h1>{name}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-// NEW: Total component to calculate the sum
-const Total = ({ parts }) => {
-  const totalAmount = parts.reduce((sum, part) => {
-    // This will print to your browser console for every item in the array
-    console.log("what is happening", sum, part);
-    return sum + part.exercises;
-  }, 0);
-
-  return <strong>total of {totalAmount} exercises</strong>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
@@ -60,7 +25,6 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {/* Exercise 2.4: Loop through the courses array */}
       {courses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
