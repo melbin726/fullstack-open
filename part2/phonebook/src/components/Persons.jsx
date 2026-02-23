b593492b4b7db3772f035a643f10a4d0a3bfbf36
melbin726 <141119619+melbin726@users.noreply.github.com>
2026-02-23 13:13:12 +0530

    2.6-2.10

---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,10 @@
+const Persons = ({ persons }) => (
+  <>
+    {persons.map((person) => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </>
+);
+export default Persons;
